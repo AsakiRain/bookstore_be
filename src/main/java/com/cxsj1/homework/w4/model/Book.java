2dded9783fdb977c6b2cfbb19c19ba37cdd4db0c
AsakiRain <1287059631@qq.com>
2022-11-25 15:30:21 +0800

    修改了数据库字段，更新了数据库转储文件

--- src/main/java/com/cxsj1/homework/w4/model/Book.java
+++ src/main/java/com/cxsj1/homework/w4/model/Book.java
@@ -9,15 +9,38 @@ public class Book {
     public String title;
     public String author;
     public String publisher;
+    public String publish_at;
+    public String page;
+    public String binding;
+    public String series;
+    public String translator;
+    public String original_title;
+    public String producer;
+    public String id;
+    public String url;
+    public String rating;
+    public String rating_people;
     public String intro;
     public String cover;
     public String price;
+
 
     public void set(BookForm bookForm) {
         this.isbn = bookForm.isbn;
         this.title = bookForm.title;
         this.author = bookForm.author;
         this.publisher = bookForm.publisher;
+        this.publish_at = bookForm.publish_at;
+        this.page = bookForm.page;
+        this.binding = bookForm.binding;
+        this.series = bookForm.series;
+        this.translator = bookForm.translator;
+        this.original_title = bookForm.original_title;
+        this.producer = bookForm.producer;
+        this.id = bookForm.id;
+        this.url = bookForm.url;
+        this.rating = bookForm.rating;
+        this.rating_people = bookForm.rating_people;
         this.intro = bookForm.intro;
         this.cover = bookForm.cover;
         this.price = bookForm.price;
@@ -28,6 +51,17 @@ public class Book {
         this.title = (String) item.get("title");
         this.author = (String) item.get("author");
         this.publisher = (String) item.get("publisher");
+        this.publish_at = (String) item.get("publish_at");
+        this.page = (String) item.get("page");
+        this.binding = (String) item.get("binding");
+        this.series = (String) item.get("series");
+        this.translator = (String) item.get("translator");
+        this.original_title = (String) item.get("original_title");
+        this.producer = (String) item.get("producer");
+        this.id = (String) item.get("id");
+        this.url = (String) item.get("url");
+        this.rating = (String) item.get("rating");
+        this.rating_people = (String) item.get("rating_people");
         this.intro = (String) item.get("intro");
         this.cover = (String) item.get("cover");
         this.price = (String) item.get("price");
@@ -38,13 +72,7 @@ public class Book {
         if (map == null) {
             return "图书不存在";
         }
-        this.isbn = (String) map.get("isbn");
-        this.title = (String) map.get("title");
-        this.author = (String) map.get("author");
-        this.publisher = (String) map.get("publisher");
-        this.intro = (String) map.get("intro");
-        this.cover = (String) map.get("cover");
-        this.price = (String) map.get("price");
+        this.set(map);
         return null;
     }
 
@@ -58,16 +86,22 @@ public class Book {
     }
 
     public boolean save() {
-        int affectedRows = DB.commit("update books set title = ?, author = ?, publisher = ?, intro = ?, cover = ?, " +
-                "price = ? where isbn = ?", this.title, this.author, this.publisher, this.intro, this.cover,
-                this.price, this.isbn);
+        int affectedRows = DB.commit("update books set title = ?, author = ?, publisher = ?, publish_at = ?, page = " +
+                "?, binding = ?, series = ?, translator = ?, original_title = ?, producer = ?, id = ?, url = ?, " +
+                "rating = ?, rating_people = ?, intro = ?, cover = ?, price = ? where isbn = ?", this.title,
+                this.author, this.publisher, this.publish_at, this.page, this.binding, this.series, this.translator,
+                this.original_title, this.producer, this.id, this.url, this.rating, this.rating_people, this.intro,
+                this.cover, this.price, this.isbn);
         return affectedRows == 1;
     }
 
     public static boolean create(BookForm bookForm) {
-        int affectedRows = DB.commit("insert into books (isbn, title, author, publisher, intro, cover, price) values " +
-                "(?, ?, ?, ?, ?, ?, ?)", bookForm.isbn, bookForm.title, bookForm.author, bookForm.publisher,
-                bookForm.intro, bookForm.cover, bookForm.price);
+        int affectedRows = DB.commit("insert into books (isbn, title, author, publisher, publish_at, page, binding, " +
+                "series, translator, original_title, producer, id, url, rating, rating_people, intro, cover, price) " +
+                "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", bookForm.isbn, bookForm.title,
+                bookForm.author, bookForm.publisher, bookForm.publish_at, bookForm.page, bookForm.binding,
+                bookForm.series, bookForm.translator, bookForm.original_title, bookForm.producer, bookForm.id,
+                bookForm.url, bookForm.rating, bookForm.rating_people, bookForm.intro, bookForm.cover, bookForm.price);
         return affectedRows == 1;
     }
 }
--- src/main/java/com/cxsj1/homework/w4/model/BookForm.java
+++ src/main/java/com/cxsj1/homework/w4/model/BookForm.java
@@ -5,6 +5,17 @@ public class BookForm {
     public String title;
     public String author;
     public String publisher;
+    public String publish_at;
+    public String page;
+    public String binding;
+    public String series;
+    public String translator;
+    public String original_title;
+    public String producer;
+    public String id;
+    public String url;
+    public String rating;
+    public String rating_people;
     public String intro;
     public String cover;
     public String price;
