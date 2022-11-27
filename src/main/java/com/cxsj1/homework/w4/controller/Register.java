165e9a957f8e980deacbca6988f3af78ae26ea1f
AsakiRain <1287059631@qq.com>
2022-11-27 15:58:49 +0800

    注册成功时将会直接返回token

--- src/main/java/com/cxsj1/homework/w4/controller/Register.java
+++ src/main/java/com/cxsj1/homework/w4/controller/Register.java
@@ -5,12 +5,15 @@ import com.cxsj1.homework.w4.model.RegisterForm;
 import com.cxsj1.homework.w4.model.User;
 import com.cxsj1.homework.w4.utils.Req;
 import com.cxsj1.homework.w4.utils.Res;
+import com.cxsj1.homework.w4.utils.Token;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet("/api/register")
 public class Register extends HttpServlet {
@@ -21,6 +24,7 @@ public class Register extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String err;
         RegisterForm registerForm = JSON.parseObject(req.getInputStream().readAllBytes(), RegisterForm.class);
         if (registerForm == null) {
             Res.Error(res, 400, 40002, "参数不足或有误");
@@ -41,11 +45,30 @@ public class Register extends HttpServlet {
             return;
         }
 
-        if (User.create(username, nickname, password, sex)) {
-            Res.Json(res, 20000, "注册成功");
-        } else {
+        if (!User.create(username, nickname, password, sex)) {
             Res.Json(res, 50000, "注册失败");
-
+            return;
         }
+
+        User user = new User();
+        err = User.get(username, user);
+        if (err != null) {
+            Res.Json(res, 42203, err);
+            return;
+        }
+
+        String token = Token.create(user.username, user.nickname, user.sex);
+        if (token == null) {
+            Res.Error(res, 500, 50000, "未能生成token");
+            return;
+        }
+
+        HashMap<String, Object> data = new HashMap<>() {
+            {
+                put("token", token);
+            }
+        };
+        res.addCookie(new Cookie("token", token));
+        Res.Json(res, 20000, "注册成功", data);
     }
 }
