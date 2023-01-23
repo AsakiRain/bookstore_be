98d67b9603d64d8b418a84908a4b49c7a9e5a76b
AsakiRain <1287059631@qq.com>
2023-01-23 21:19:55 +0800

    chore: 添加Order和Statistic类型

---
+++ src/main/java/com/cxsj1/homework/w5/model/Order.java
@@ -0,0 +1,12 @@
+package com.cxsj1.homework.w5.model;
+
+public class Order {
+    public String serial;
+    public String username;
+    public String cost;
+    public String isbn;
+    public String status;
+    public String created_at;
+    public String updated_at;
+    public String finished_at;
+}
---
+++ src/main/java/com/cxsj1/homework/w5/model/Statistic.java
@@ -0,0 +1,18 @@
+package com.cxsj1.homework.w5.model;
+
+public class Statistic {
+    public java.util.Date date;
+
+    public int deal_income;
+    public int deal_count;
+    public int purchase_count;
+    public int page_view;
+    public int goods_view;
+    public int new_goods_count;
+    public int goods_count;
+    public int total_view;
+    public int new_user_count;
+    public int user_count;
+    public int created_at;
+    public int updated_at;
+}
