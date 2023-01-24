601a78da69206a8f9e2991d218c464ab923e4bae
AsakiRain <1287059631@qq.com>
2023-01-24 14:26:49 +0800

    chore: 将CORS配置项移入config

--- src/main/java/com/cxsj1/homework/w5/Config.java
+++ src/main/java/com/cxsj1/homework/w5/Config.java
@@ -2,14 +2,23 @@ package com.cxsj1.homework.w5;
 
 public class Config {
 
-    public static final class JWT{
+    public static final class JWT {
         public static final String SECRET = "😨世界，遗忘我😭";
         public static final String ISSUER = "rainchen";
     }
-    public static final class DB{
+
+    public static final class DB {
         public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-        public static final String URL = "jdbc:mysql://localhost:6033/bookstore?autoReconnect=true&useSSL=false&useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai";
+        public static final String URL = "jdbc:mysql://localhost:6033/bookstore?autoReconnect=true&useSSL=false" +
+                "&useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai";
         public static final String USER = "crud";
         public static final String PASS = "ssr129631";
+    }
+
+    public static final class CORS_CONFIG {
+        public static final String ALLOW_ORIGIN = "*";
+        public static final String ALLOW_METHODS = "GET, POST, PUT, DELETE, OPTIONS";
+        public static final String ALLOW_HEADERS = "Content-Type, Authorization";
+        public static final String ALLOW_CREDENTIALS = "true";
     }
 }
--- src/main/java/com/cxsj1/homework/w5/filter/CORS.java
+++ src/main/java/com/cxsj1/homework/w5/filter/CORS.java
@@ -2,22 +2,24 @@ package com.cxsj1.homework.w5.filter;
 
 import java.io.IOException;
 
+import com.cxsj1.homework.w5.Config;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import com.cxsj1.homework.w5.Config.CORS_CONFIG;
 
 @WebFilter(filterName = "CORS", urlPatterns = "/*")
 public class CORS implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest  req = (HttpServletRequest) servletRequest;
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse res = (HttpServletResponse) servletResponse;
         System.out.println("::CORS at ".concat(req.getRequestURI()));
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Origin", CORS_CONFIG.ALLOW_ORIGIN);
+        res.setHeader("Access-Control-Allow-Methods", CORS_CONFIG.ALLOW_METHODS);
+        res.setHeader("Access-Control-Allow-Headers", CORS_CONFIG.ALLOW_HEADERS);
+        res.setHeader("Access-Control-Allow-Credentials", CORS_CONFIG.ALLOW_CREDENTIALS);
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
